47ca5b239c149e6890a88003766079bf177dcc8f
LINUX-PC <jesusrope98@outlook.com>
2019-12-14 16:52:37 +0100

    Updated deps

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\conta\Downloads\flutter_flutter_qr_scanner\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/home/jesus/.flutter-sdk"
+export "FLUTTER_APPLICATION_PATH=/home/jesus/Documents/packs/flutter_qr_scanner/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/jesus/.flutter-sdk/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
